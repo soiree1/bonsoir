5fbf19836ede52f20b068a62db423f8e8d537f24
Synthbot Anon <synthbot@celestia.ai>
2023-10-08 14:46:51 -0700

    move modules.py to __init__.py
    bugfix: the default imports were in the wrong file.

---
+++ src/eqmesh/__init__.py
@@ -0,0 +1,4 @@
+from .gptbase_module import GPTBaseModule
+from .chatgpt_module import ChatGPTModule
+from .chatbase_module import ChatBaseModule
+
--- src/eqmesh/modules.py
+++
@@ -1,4 +0,0 @@
-from .gptbase_module import GPTBaseModule
-from .chatgpt_module import ChatGPTModule
-from .chatbase_module import ChatBaseModule
-
